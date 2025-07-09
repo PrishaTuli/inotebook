1eb99d61ab455b0f71c9c0d97d7ae0438ef179cf
PrishaTuli <prishatuli007@gmail.com>
2025-07-09 15:32:41 +0530

    Notestate.js

--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -3,26 +3,6 @@ import Notecontext from "./Notecontext";
 import { useState } from "react";
 const Notestate = (props) => {
   const host = "http://localhost:5000";
-  // const notesinitial = [
-  //   {
-  //     _id: "66cdcc776cead93e785d587a",
-  //     user: "66cade131996fd08881cb1d3",
-  //     title: "my title",
-  //     description: "please wake up early",
-  //     tag: "personal",
-  //     date: "2024-08-27T12:54:15.409Z",
-  //     __v: 0,
-  //   },
-  //   {
-  //     _id: "66d80a59f7c575e86d3f4b48",
-  //     user: "66cade131996fd08881cb1d3",
-  //     title: "my title pretty1",
-  //     description: "please wake up early1",
-  //     tag: "personal",
-  //     date: "2024-09-04T07:20:57.412Z",
-  //     __v: 0,
-  //   },
-  // ];
   const notesinitial=[];
   const [notes, setnotes] = useState(notesinitial);
   //get note
@@ -50,17 +30,6 @@ const Notestate = (props) => {
     });
     const note=await response.json();
     setnotes(notes.concat(note));
-    // const note = {
-    //   _id: "66d80a59f7c575e86d3f4b428",
-    //   user: "66cade131996fd08881cb1d3",
-    //   title: title,
-    //   description: description,
-    //   tag: tag,
-    //   date: "2024-09-04T07:20:57.412Z",
-    //   __v: 0,
-    // };
-    // setnotes(notes.concat(note));
-  };
   const deletenote = async (id) => {
     const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
       method: "DELETE",
@@ -100,27 +69,7 @@ const Notestate = (props) => {
     }
     setnotes(newnotes);
   };
-  // const s1={
-  //     "name":"harry",
-  //     "class":"5b"
-  // }
-  // const[state,setstate]=useState(s1);
-  // const update=()=>{
-  //     setTimeout(()=>{
-  //         setstate({
-  //             "name":"larry",
-  //             "class":"10b"
-  //         })
-  //     },1000);
-  // }
   return (
-    // <Notecontext.Provider value={{state,update}}>
-    //     {props.children}
-    // </Notecontext.Provider>
-    //
-    // <Notecontext.Provider value={{notes,setnotes}}>
-    //     {props.children}
-    // </Notecontext.Provider>
     <Notecontext.Provider value={{ notes, addnote, deletenote, editnote ,getnotes}}>
       {props.children}
     </Notecontext.Provider>
