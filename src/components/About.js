87e4a8378d4f9f60a052f9a3e49eaefe48a1051b
PrishaTuli <prishatuli007@gmail.com>
2025-07-09 15:30:47 +0530

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,9 @@ import React, { useEffect } from 'react';
 import {useContext} from 'react';
 import Notecontext from '../context/notes/Notecontext';
 const About = () => {
-  // const a =useContext(Notecontext);
-  // useEffect(()=>{
-  //   a.update();
-    
-  // },[])
   return (
     <div>
-      {/* this is about {a.state.name} */}
+      
     </div>
   )
 }
