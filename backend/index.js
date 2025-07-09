6ebca87eb7914059c815c574917800728f095a68
PrishaTuli <prishatuli007@gmail.com>
2025-07-09 15:29:52 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,10 @@
-// const connectto=require('./db');
-// connectto();
+
 const getconnection=require('./db');
 const express = require('express');
 var cors=require('cors');
  getconnection();
  
 const app = express();
-// app.get('/', (req, res) => {
-//     try {
-//       res.send("hello");
-//     } catch (error) {
-//       console.error('An error occurred:', error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth') )
